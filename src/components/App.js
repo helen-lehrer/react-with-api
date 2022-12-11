76db2ee8d34e8cf695758575785c4c665c883574
Helen Lehrer <hlehrer@outlook.com>
2022-12-11 15:00:29 -0800

    Add Top Stories api call

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TopStories from './TopStories';
+
+const App = () => {
+  return (
+    <>
+      < TopStories />
+    </>
+  );
+}
+
+export default App;
